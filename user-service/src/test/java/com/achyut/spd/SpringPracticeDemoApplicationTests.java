a1a2d7c8d5dc404ec6e9c5177871f588e382b6e7
achyut-z <achyutzalavadia123@gmail.com>
2024-07-10 14:28:41 +0530

    Rename source package

---
+++ user-service/src/test/java/com/achyut/spd/SpringPracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achyut.spd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
