5c6df05bfba055d4d8c23b665e16483fca4b99a1
Achyut <achyutzalavadia123@gmail.com>
2024-08-18 23:28:38 +0530

    Create class for constants for exception

---
+++ user-service/src/main/java/com/achyut/spd/userservice/exceptions/ExceptionConstants.java
@@ -0,0 +1,9 @@
+package com.achyut.spd.userservice.exceptions;
+
+public class ExceptionConstants {
+
+    public static final String USERNAME_BLANK = "Username cannot be blank";
+
+    public static final String USERNAME_TAKEN = "Username is taken. Please choose another";
+
+}
