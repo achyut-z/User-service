132f72d1876a4395c03cff7fe6fe2b3966f3b46c
Achyut <achyutzalavadia123@gmail.com>
2024-08-26 00:29:59 +0530

    Create PasswordValidator

--- user-service/src/main/java/com/achyut/spd/userservice/constants/GlobalConstants.java
+++ user-service/src/main/java/com/achyut/spd/userservice/constants/GlobalConstants.java
@@ -2,6 +2,10 @@ package com.achyut.spd.userservice.constants;
 
 public class GlobalConstants {
 	
-	public static final String NOT_FOUND = "NOT FOUND";
+	public static final String USER_NOT_FOUND = "USER NOT FOUND";
+	
+    public static final String SAME_PASSWORD = "New password cannot be same as old password";
+    
+    public static final String NOT_ENOUGH_LENGTH = "Length needs to be atleast 8 characters";
 
 }
