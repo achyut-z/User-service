9cb9f832e5baeb74dfea6415de6b9f7c4d8bcbac
achyut-z <achyutzalavadia123@gmail.com>
2024-09-05 19:45:52 +0530

    Remove unnecessary code

--- user-service/src/main/java/com/achyut/spd/userservice/services/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/achyut/spd/userservice/services/impl/UserServiceImpl.java
@@ -1,32 +1,22 @@
 package com.achyut.spd.userservice.services.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.achyut.spd.generator.PasswordGenerator;
 import com.achyut.spd.mapper.GenericDynamicMapper;
 import com.achyut.spd.userservice.constants.ExceptionConstants;
 import com.achyut.spd.userservice.constants.GlobalConstants;
-import com.achyut.spd.userservice.dtos.AddressDto;
 import com.achyut.spd.userservice.dtos.CredentialDto;
-import com.achyut.spd.userservice.dtos.UserDetailsDto;
 import com.achyut.spd.userservice.dtos.request.ChangePassword;
 import com.achyut.spd.userservice.dtos.request.CreateUserRequest;
 import com.achyut.spd.userservice.dtos.request.SetPassword;
 import com.achyut.spd.userservice.dtos.response.CredentialResponse;
 import com.achyut.spd.userservice.dtos.response.UserResponse;
-import com.achyut.spd.userservice.entities.Address;
 import com.achyut.spd.userservice.entities.Credentials;
 import com.achyut.spd.userservice.entities.User;
-import com.achyut.spd.userservice.entities.UserDetails;
 import com.achyut.spd.userservice.repositories.UserRepository;
 import com.achyut.spd.userservice.services.UserService;
-import com.achyut.spd.validator.EmailValidator;
 import com.achyut.spd.validator.PasswordValidator;
-import com.achyut.spd.validator.PhoneNumberValidator;
-import com.achyut.spd.validator.UsernameValidator;
 import com.achyut.spd.validator.request.CreateUserRequestValidator;
 
 import io.micrometer.common.util.StringUtils;
@@ -40,22 +30,13 @@ public class UserServiceImpl implements UserService {
 
     private final GenericDynamicMapper<Credentials, CredentialDto> credentialMapper;
 
-    private final GenericDynamicMapper<UserDetails, UserDetailsDto> detailsMapper;
-
-    private final GenericDynamicMapper<Address, AddressDto> addressMapper;
-
-    @Autowired
     public UserServiceImpl(UserRepository userRepository,
             GenericDynamicMapper<User, CreateUserRequest> mapper,
-            GenericDynamicMapper<Credentials, CredentialDto> credentialMapper,
-            GenericDynamicMapper<UserDetails, UserDetailsDto> detailsMapper,
-            GenericDynamicMapper<Address, AddressDto> addressMapper) {
+            GenericDynamicMapper<Credentials, CredentialDto> credentialMapper) {
 
         this.userRepository = userRepository;
         this.mapper = mapper;
         this.credentialMapper = credentialMapper;
-        this.detailsMapper = detailsMapper;
-        this.addressMapper = addressMapper;
 
     }
 
