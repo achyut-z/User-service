823df068c0460745a3690d7b5a33bc5578686804
achyut-z <achyutzalavadia123@gmail.com>
2024-10-01 17:00:25 +0530

    Format UserController.java

--- user-service/src/main/java/com/userservice/controller/UserController.java
+++ user-service/src/main/java/com/userservice/controller/UserController.java
@@ -1,90 +1,59 @@
 package com.userservice.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.userservice.dto.request.ChangePassword;
 import com.userservice.dto.request.CreateUserRequest;
 import com.userservice.dto.request.SetPassword;
 import com.userservice.dto.response.CredentialResponse;
 import com.userservice.dto.response.UserResponse;
 import com.userservice.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/")
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	public UserController(UserService userService) {
+    public UserController(UserService userService) {
 
-		this.userService = userService;
+        this.userService = userService;
 
-	}
+    }
 
-	@PostMapping("create")
-	public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request) {
+    @PostMapping("create")
+    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request) {
 
-		try {
+        UserResponse user = this.userService.createUser(request);
 
-			UserResponse user = this.userService.createUser(request);
+        return new ResponseEntity<>(user, HttpStatus.OK);
 
-			return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
-		}
-		catch (IllegalArgumentException e) {
+    @GetMapping("{username}")
+    public ResponseEntity<?> getUserByUsername(@PathVariable("username") String username) {
 
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-	}
+        CredentialResponse userCredentials = this.userService.getUserByUsername(username);
 
-	@GetMapping("{username}")
-	public ResponseEntity<?> getUserByUsername(@PathVariable("username") String username) {
+        return new ResponseEntity<>(userCredentials, HttpStatus.OK);
+    }
 
-		CredentialResponse userCredentials = this.userService.getUserByUsername(username);
+    @PutMapping("/setPassword")
+    public ResponseEntity<?> setPassword(@RequestBody SetPassword request) {
 
-		ResponseEntity<?> response = new ResponseEntity<>(userCredentials, HttpStatus.OK);
-		return response;
-	}
+        UserResponse credentials = this.userService.setPasswordForRegisteredUser(request);
 
-	@PutMapping("/setPassword")
-	public ResponseEntity<?> setPassword(@RequestBody SetPassword request) {
+        return new ResponseEntity<>(credentials, HttpStatus.OK);
+    }
 
-		try {
+    @PostMapping("/changePassword")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePassword request) {
 
-			UserResponse credentials = this.userService.setPasswordForRegisteredUser(request);
+        CredentialResponse credentials = this.userService.changePassword(request);
 
-			return new ResponseEntity<>(credentials, HttpStatus.OK);
+        return new ResponseEntity<>(credentials, HttpStatus.OK);
 
-		}
-		catch (IllegalArgumentException e) {
-
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-
-	}
-
-	@PostMapping("/changePassword")
-	public ResponseEntity<?> changePassword(@RequestBody ChangePassword request) {
-
-		try {
-
-			CredentialResponse credentials = this.userService.changePassword(request);
-
-			return new ResponseEntity<>(credentials, HttpStatus.OK);
-
-		}
-		catch (IllegalArgumentException e) {
-
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-	}
+    }
 
 }
